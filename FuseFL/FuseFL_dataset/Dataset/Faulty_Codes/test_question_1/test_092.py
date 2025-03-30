3a0ac3013ad353ec5db63e70d3501e0873345076
玛卡巴卡 <2200013153@stu.pku.edu.cn>
2025-03-30 11:05:16 +0800

    summary 3.30

---
+++ FuseFL/FuseFL_dataset/Dataset/Faulty_Codes/test_question_1/test_092.py
@@ -0,0 +1,34 @@
+from questions.question_1.code.wrong.wrong_1_092 import *
+def test_001():
+    assert search(42, (-5, 1, 3, 5, 7, 10)) == 6
+
+def test_002():
+    assert search(42, [1, 5, 10]) == 3
+
+def test_003():
+    assert search(5, (1, 5, 10)) == 1
+
+def test_004():
+    assert search(7, [1, 5, 10]) == 2
+
+def test_005():
+    assert search(3, (1, 5, 10)) == 1
+
+def test_006():
+    assert search(-5, (1, 5, 10)) == 0
+
+def test_007():
+    assert search(10, (-5, -1, 3, 5, 7, 10)) == 5
+
+def test_008():
+    assert search(-100, (-5, -1, 3, 5, 7, 10)) == 0
+
+def test_009():
+    assert search(0, (-5, -1, 3, 5, 7, 10)) == 2
+
+def test_010():
+    assert search(100, []) == 0
+
+def test_011():
+    assert search(-100, ()) == 0
+
---
+++ FuseFL/FuseFL_dataset/Dataset/Faulty_Codes/test_question_3/test_172.py
@@ -0,0 +1,19 @@
+from questions.question_3.code.wrong.wrong_3_172 import *
+def test_001():
+    assert remove_extras([1, 1, 1, 2, 3]) == [1, 2, 3]
+
+def test_002():
+    assert remove_extras([1, 5, 1, 1, 3, 2]) == [1, 5, 3, 2]
+
+def test_003():
+    assert remove_extras([]) == []
+
+def test_004():
+    assert remove_extras([3, 4, 5, 1, 3]) == [3, 4, 5, 1]
+
+def test_005():
+    assert remove_extras([3, 4, 5, 1, 3]) == [3, 4, 5, 1]
+
+def test_006():
+    assert remove_extras([3, 4, 5, 1, 3]) == [3, 4, 5, 1]
+
